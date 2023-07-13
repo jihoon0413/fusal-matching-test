b79da25cd7875f94cdc19a056d1f77b26c1aee17
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-07-13 14:18:07 +0900

    #1 - Spring Boot  생성

---
+++ backend/src/main/java/com/example/fusalmatching/FusalMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.fusalmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusalMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusalMatchingApplication.class, args);
+	}
+
+}
