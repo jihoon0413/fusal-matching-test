18b0a96afdcc4a28a53964fe9bad66474074a35f
minji-2219 <aabb0219@naver.com>
2023-07-18 00:58:06 +0900

    #20 - list info component만들기
    list info 컴포넌트 한 덩어리 만들고 메인 페이지에 배치시키기

--- frontend/src/components/InfoList.js
+++ frontend/src/components/InfoList.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import ImgSwiper from './ImgSwiper'
+import '../css/components/InfoList.css'
+import { Link } from 'react-router-dom'
 
 const InfoList = () => {
   return (
     <div>
-      <ImgSwiper width={300} height={200}/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      <div className='card'>
+        <ImgSwiper width={320} height={250}/>
+        <div className='description'>
+            <div className='head'>
+              <div className='name'><Link to='/info/:index'>신화 풋살장</Link></div>
+              <div className='line'></div>
+              <div className='price'>2시간 ￦80,000</div>
+              <div><button><Link to='/reserve'>예약하기</Link></button></div>
+            </div>
+
+            <div className='address'>광주광역시 광산구 산정동 15</div>
+            <div className='time'><span style={{fontSize:"20px"}} className="material-symbols-outlined">history</span> 이용시간대 13:00~23:00</div>
+            <div className='phone'><span style={{fontSize:"20px"}} className="material-symbols-outlined">phone_in_talk</span> 062-555-555</div>
+
+          <div className='round_sticker'>
+            <div className='holiday' style={{width:"80px", height:"80px"}}>연중<br/>무휴</div>
+            <div className='parking' style={{width:"80px", height:"80px"}}><span class="material-symbols-outlined">local_parking</span></div>
+            <div className='review_count' style={{width:"80px", height:"80px"}}></div>
+          </div>
+        </div>
+      </div>
+      <hr style={{width:"1300px"}}/>
     </div>
   )
 }
--- frontend/src/pages/ListPage.js
+++ frontend/src/pages/ListPage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import InfoList from '../components/InfoList'
+import '../css/pages/ListPage.css'
 
 const ListPage = () => {
+
   return (
-    <div>
-      ListPage입니다
+    <div className='center'>
+      <div>구장의 자세한 정보가 궁금하다면? click !</div>
       <InfoList/>
+      <InfoList/>
+      <InfoList/>
+
     </div>
   )
 }
