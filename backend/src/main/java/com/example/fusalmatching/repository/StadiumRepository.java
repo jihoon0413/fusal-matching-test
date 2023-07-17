798067a5598c9ae7cb4c83201ac257c933088f77
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-07-17 23:30:50 +0900

    #14 - 레파지토리 생성
    JpaRepository 를 implements한 각 테이블에 레파지토리 인터페이스 생성

---
+++ backend/src/main/java/com/example/fusalmatching/repository/StadiumRepository.java
@@ -0,0 +1,8 @@
+package com.example.fusalmatching.repository;
+
+import com.example.fusalmatching.domain.Field;
+import com.example.fusalmatching.domain.Stadium;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+}
