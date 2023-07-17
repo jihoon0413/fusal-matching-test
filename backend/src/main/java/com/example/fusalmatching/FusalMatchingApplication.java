8b6d76656924f2f667fb18e23a5f1d97baec1651
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-07-17 23:32:23 +0900

    #14 - 오타 수정
    각각의 다른 이름으로 오류가 일어나서 해결
    MatchingRecord의 index의 경우 고민이 필요해서 주석처리

--- backend/src/main/java/com/example/fusalmatching/FusalMatchingApplication.java
+++ backend/src/main/java/com/example/fusalmatching/FusalMatchingApplication.java
@@ -2,8 +2,10 @@ package com.example.fusalmatching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FusalMatchingApplication {
 
 	public static void main(String[] args) {
