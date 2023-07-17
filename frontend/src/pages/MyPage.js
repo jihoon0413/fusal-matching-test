480e1c32847ac269123cc80c7e04e6ace41d9239
minji2219 <119796600+minji2219@users.noreply.github.com>
2023-07-17 14:00:49 +0900

    page 폴더 나누기

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const dummyfile = () => {
+const MyPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const dummyfile = () => {
   )
 }
 
-export default dummyfile
+export default MyPage
