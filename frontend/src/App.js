f6061e02139acdbfe41b2d536a18a9206851c627
minji-2219 <aabb0219@naver.com>
2023-07-17 15:44:33 +0900

    #16 - router로 page 이동하게 만들기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import MenuBar from './components/MenuBar';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import InfoPage from './pages/InfoPage';
+import ReservePage from './pages/ReservePage';
+import MyPage from './pages/MyPage';
+import LoginPage from './pages/LoginPage';
 
+
+const Layout =()=>{
+  return(
+    <>
+    <MenuBar/>
+    <Outlet/>
+    </>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <MenuBar/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<ListPage/>}/>
+          <Route path="/info/:input" element={<InfoPage/>}/>
+          <Route path='/reserve' element={<ReservePage/>}/>
+          <Route path='/mypage' element={<MyPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../css/components/MenuBar.css'
+import { Link } from 'react-router-dom'
 
 const MenuBar = () => {
   return (
     <div>
       <div className='menu'>
         <ul>
-          <li className='logo'>광주풋살</li>
-          <li className='info'>구장 정보</li>
-          <li className='reserve'>예약하기</li>
-          <li className='login'><button>로그인</button></li>
+          <li className='logo'><Link to='/'>광주 풋살</Link></li>
+          <li className='info'><Link to='/'>구장 정보</Link></li>
+          <li className='reserve'><Link to='reserve'>예약하기</Link></li>
+          <li className='login'><Link to='login'><button>로그인</button></Link></li>
         </ul>
       </div>
     </div>
