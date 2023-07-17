9646735a38bca89a84203a6b1774cb9bebf1decd
minji2219 <200146@jnu.ac.kr>
2023-07-17 22:28:06 +0900

    swiper 모듈 다운, 크기 조정, 설정

---
+++ frontend/src/components/InfoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImgSwiper from './ImgSwiper'
+
+const InfoList = () => {
+  return (
+    <div>
+      <ImgSwiper width={300} height={200}/>
+    </div>
+  )
+}
+
+export default InfoList
