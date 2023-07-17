f3d83fac571f99b118cbf2770ee83ef2f3f5b6c1
minji-2219 <aabb0219@naver.com>
2023-07-17 22:28:06 +0900

    #18 - 이미지 swiper만들기
    swiper 모듈 다운, 크기 조정, 설정

--- frontend/src/pages/ListPage.js
+++ frontend/src/pages/ListPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import InfoList from '../components/InfoList'
 
 const ListPage = () => {
   return (
     <div>
       ListPage입니다
+      <InfoList/>
     </div>
   )
 }
