396d599a937651e9c2d49c65174e1620a484e808
minji-2219 <aabb0219@naver.com>
2023-07-18 23:00:37 +0900

    #23 - useContext 세팅

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import StadiumInfoProvider from './context/StadiumInfoContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <StadiumInfoProvider>
     <App />
+  </StadiumInfoProvider>
   </BrowserRouter>
 );
 
