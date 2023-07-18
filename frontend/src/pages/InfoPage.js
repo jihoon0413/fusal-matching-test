603e0ce5e5dfa57c3932ed5bf211306597ab1447
minji-2219 <aabb0219@naver.com>
2023-07-19 00:07:06 +0900

    #23 - infoPage 중 swiper, 구장 설명 ui완성
    구장 설명 ui중 사장님 사진 위치 해결 필요

--- frontend/src/pages/InfoPage.js
+++ frontend/src/pages/InfoPage.js
@@ -1,13 +1,41 @@
 import React, { useContext } from 'react'
 import { StadiumInfoContext } from '../context/StadiumInfoContext'
+import ImgSwiper from '../components/ImgSwiper'
+import '../css/pages/InfoPage.css'
+import img from '../img/사장님.jpg'
 
 const InfoPage = () => {
 
   const {value} = useContext(StadiumInfoContext)
-  console.log(value)
+
 
   return (
     <div>
+       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      <div className='center'>
+        <ImgSwiper width={1300} height={400}/>
+        <hr/>
+        <div className='infos'>
+          <div className='infos_stadium'>
+            <div className="description">
+              <div className='name' style={{marginBottom:'20px', fontSize:'40px'}}>{value.name}</div>
+              <div className='address' style={{marginBottom:'10px', fontSize:'20px'}}>{value.address}</div>
+              <div className='time'><span style={{fontSize:"20px"}} className="material-symbols-outlined">history</span> 이용시간대 {value.time}</div>
+              <div className='phone'><span style={{fontSize:"20px"}} className="material-symbols-outlined">phone_in_talk</span> {value.phone}</div>
+            </div>
+            <div className=''>
+              <img className='master_img' src={img} alt='사장님'/>
+              <div className=''>신사장님</div>
+              <div className='round_sticker'>
+                <div className='holiday' style={{width:"80px", height:"80px"}}>연중<br/>무휴</div>
+                <div className='parking' style={{width:"80px", height:"80px"}}><span className="material-symbols-outlined">local_parking</span></div>
+                <div className='review_count' style={{width:"80px", height:"80px"}}></div>
+              </div>
+            </div>
+          </div>
+          <div className='infos_review'></div>
+        </div>
+      </div>
     </div>
   )
 }
