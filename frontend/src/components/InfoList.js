deb557b2e4b2d91ac9146426c85e7b79bfadb4b1
minji2219 <200146@jnu.ac.kr>
2023-07-19 00:07:06 +0900

    구장 설명 ui중 사장님 사진 위치 해결 필요

--- frontend/src/components/InfoList.js
+++ frontend/src/components/InfoList.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ImgSwiper from './ImgSwiper'
 import '../css/components/InfoList.css'
 import { Link } from 'react-router-dom'
+import { StadiumInfoContext } from '../context/StadiumInfoContext'
 
 const InfoList = () => {
+  const {value} = useContext(StadiumInfoContext)
   return (
     <div>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
@@ -11,19 +13,19 @@ const InfoList = () => {
         <ImgSwiper width={320} height={250}/>
         <div className='description'>
             <div className='head'>
-              <div className='name'><Link to='/info/1'>신화 풋살장</Link></div>
+              <div className='name'><Link to='/info/1'>{value.name}</Link></div>
               <div className='line'></div>
-              <div className='price'>2시간 ￦80,000</div>
+              <div className='price'>{value.price}</div>
               <div><button><Link to='/reserve'>예약하기</Link></button></div>
             </div>
 
-            <div className='address'>광주광역시 광산구 산정동 15</div>
-            <div className='time'><span style={{fontSize:"20px"}} className="material-symbols-outlined">history</span> 이용시간대 13:00~23:00</div>
-            <div className='phone'><span style={{fontSize:"20px"}} className="material-symbols-outlined">phone_in_talk</span> 062-555-555</div>
+            <div className='address'>{value.address}</div>
+            <div className='time'><span style={{fontSize:"20px"}} className="material-symbols-outlined">history</span> 이용시간대 {value.time}</div>
+            <div className='phone'><span style={{fontSize:"20px"}} className="material-symbols-outlined">phone_in_talk</span>{value.phone}</div>
 
           <div className='round_sticker'>
             <div className='holiday' style={{width:"80px", height:"80px"}}>연중<br/>무휴</div>
-            <div className='parking' style={{width:"80px", height:"80px"}}><span class="material-symbols-outlined">local_parking</span></div>
+            <div className='parking' style={{width:"80px", height:"80px"}}><span className="material-symbols-outlined">local_parking</span></div>
             <div className='review_count' style={{width:"80px", height:"80px"}}></div>
           </div>
         </div>
