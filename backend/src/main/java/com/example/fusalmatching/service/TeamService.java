39a0ac6da022266f2a212dbfe3cb1ce506546f0d
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-07-19 15:29:55 +0900

    #27 - 암호화코드 작성
    bcrypt를 통해 비밀번호를 암호화 하였다.

--- backend/src/main/java/com/example/fusalmatching/service/TeamService.java
+++ backend/src/main/java/com/example/fusalmatching/service/TeamService.java
@@ -3,8 +3,11 @@ package com.example.fusalmatching.service;
 import com.example.fusalmatching.domain.Team;
 import com.example.fusalmatching.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,10 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void createTeam(int id, String password, String teamName) {
-        System.out.println("....>>>" + id + "//" + password + "//" + teamName);
-        Team newTeam = Team.of(String.valueOf(id), password, teamName);
+
+        String hashedPassword = passwordEncoder.encode(password);
+
+        Team newTeam = Team.of(String.valueOf(id), teamName, hashedPassword);
+        System.out.println("********>>>>>>> " + newTeam.toString());
         teamRepository.save(newTeam);
 
     }
