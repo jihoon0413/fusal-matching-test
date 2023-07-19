f5746dd37523c2bcd4ac27931dd3e1793f620483
minji-2219 <aabb0219@naver.com>
2023-07-19 11:05:54 +0900

    #25 - review component 추가

---
+++ frontend/src/components/Review.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/components/Review.css'
+const Review = () => {
+  return (
+    <>
+    <div>
+      <div className='review'>
+        <div style={{marginRight:"5px", fontWeight:"700"}}className='nickname'>최강기아</div>
+        <div style={{marginRight:"10px"}}className='review_emoji'>😎</div>
+        <div className='review_content'>구장이 예뻐요</div>
+      </div>
+      <hr style={{width:"calc(1300px / 2 - 150px", marginLeft:"10px"}}/>
+    </div>
+    </>
+  )
+}
+
+export default Review
