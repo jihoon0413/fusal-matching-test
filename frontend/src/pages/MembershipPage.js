e08df3d938bca1de204f6cf8995565ba2bb749b7
minji-2219 <aabb0219@naver.com>
2023-07-21 00:39:08 +0900

    #31 - memebership페이지 만들기

---
+++ frontend/src/pages/MembershipPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MembershipPage = () => {
+  return (
+    <div>
+      <div className='membershipbox'>
+        <div>회원 가입</div>
+
+      </div>
+    </div>
+  )
+}
+
+export default MembershipPage
