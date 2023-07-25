282c5ff59aa8f374ca1e96a5c0e4f6db843282bd
minji-2219 <aabb0219@naver.com>
2023-07-25 15:04:57 +0900

    #40 - http통신 성공

--- frontend/src/components/InfoList.js
+++ frontend/src/components/InfoList.js
@@ -2,10 +2,13 @@ import React, { useContext } from 'react'
 import ImgSwiper from './ImgSwiper'
 import '../css/components/InfoList.css'
 import { Link } from 'react-router-dom'
+import axios from "axios";
 import { StadiumInfoContext } from '../context/StadiumInfoContext'
 
-const InfoList = () => {
-  const {value} = useContext(StadiumInfoContext)
+
+const InfoList = ({id,price,name,address,time,phone}) => {
+
+  // const {value} = useContext(StadiumInfoContext)
   return (
     <div>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
@@ -13,15 +16,23 @@ const InfoList = () => {
         <ImgSwiper width={320} height={250}/>
         <div className='description'>
             <div className='head'>
-              <div className='name'><Link to='/info/1'>{value.name}</Link></div>
+              <div className='name'>
+                <Link to={`/info/${id}`}
+                    state={{
+                    name:name,
+                    time:time,
+                    phone:phone,
+                    address:address,
+                  }}
+              >{name}</Link></div>
               <div className='line'></div>
-              <div className='price'>{value.price}</div>
+              <div className='price'>2시간 ￦{price}</div>
               <div><button><Link to='/reserve'>예약하기</Link></button></div>
             </div>
 
-            <div className='address'>{value.address}</div>
-            <div className='time'><span style={{fontSize:"20px"}} className="material-symbols-outlined">history</span> 이용시간대 {value.time}</div>
-            <div className='phone'><span style={{fontSize:"20px"}} className="material-symbols-outlined">phone_in_talk</span> {value.phone}</div>
+            <div className='address'>{address}</div>
+            <div className='time'><span style={{fontSize:"20px"}} className="material-symbols-outlined">history</span> 이용시간대 {time}</div>
+            <div className='phone'><span style={{fontSize:"20px"}} className="material-symbols-outlined">phone_in_talk</span> {phone}</div>
 
           <div className='round_sticker'>
             <div className='holiday' style={{width:"80px", height:"80px"}}>연중<br/>무휴</div>
