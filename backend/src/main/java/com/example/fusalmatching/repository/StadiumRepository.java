da4ba92e18ce251e55833fb0fcd1b2a99edddf26
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-07-26 17:00:02 +0900

    자신관 연관된 여러개의 데이터를 조회하기 위해 findAllByTableId 사용

--- backend/src/main/java/com/example/fusalmatching/repository/StadiumRepository.java
+++ backend/src/main/java/com/example/fusalmatching/repository/StadiumRepository.java
@@ -4,5 +4,6 @@ import com.example.fusalmatching.domain.Field;
 import com.example.fusalmatching.domain.Stadium;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface StadiumRepository extends JpaRepository<Stadium, Long> {
 }
--- backend/src/main/java/com/example/fusalmatching/repository/StadiumReviewRepository.java
+++ backend/src/main/java/com/example/fusalmatching/repository/StadiumReviewRepository.java
@@ -4,5 +4,8 @@ import com.example.fusalmatching.domain.Field;
 import com.example.fusalmatching.domain.StadiumReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StadiumReviewRepository extends JpaRepository<StadiumReview, Long> {
+    List<StadiumReview> findAllByStadiumId(Long id);
 }
