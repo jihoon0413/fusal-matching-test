7b703c290517881720f6b7176d0fedd18e69391a
minji-2219 <aabb0219@naver.com>
2023-07-29 01:27:06 +0900

    #45 - fieldComponent modal만들기
    modal창 위치 설정, modal시 뒷 배경 까매지게 설정 해야함

--- frontend/src/components/Field.js
+++ frontend/src/components/Field.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../img/Group 84.png'
 import '../css/components/Field.css'
+import Modal from './Modal'
 
-const Field = () => {
+const Field = ({fieldName , endTime, startTime, date}) => {
+
+  const [reserveSort,setReserveSort] = useState()
+  const [modalState,setModalState] = useState(false)
+
+  const MatchingReserve = ()=>{
+    setReserveSort("매칭 신청")
+    setModalState(true)
+  }
+  const WholdReserve = ()=>{
+    setReserveSort("전체 대여")
+    setModalState(true)
+  }
+
   return (
     <div className='field'>
-      <div className='field_name'>A구장</div>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      <div className='field_name'>{fieldName}구장</div>
       <div className='field_background'>
         <div className='field_main'>
           <img src={img} alt='field' className='field_img'/>
           <div className='matching_team'></div>
-          <button className='matching_btn'>매칭 신청</button>
+          <button className='matching_btn' onClick={MatchingReserve}>매칭 신청</button>
         </div>
       </div>
-      <button className='whole_btn'>전체 대여</button>
+      <button className='whole_btn' onClick={WholdReserve}>전체 대여</button>
+
+      {(modalState)
+      ?<Modal setModalState={setModalState} reserveSort={reserveSort} fieldName={fieldName} date={date} startTime={startTime} endTime={endTime}/>
+      :<></>
+      }
+      
+
     </div>
   )
 }
