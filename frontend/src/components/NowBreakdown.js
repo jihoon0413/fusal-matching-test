57c790bee065a608e4ebbb1b4062932f3f8a25f4
minji-2219 <aabb0219@naver.com>
2023-07-31 23:01:43 +0900

    #48 - mypage top완성

---
+++ frontend/src/components/NowBreakdown.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const NowBreakdown = () => {
+  return (
+    <li>
+      <span style={{margin:'8px'}}>23.07.20</span> 
+      신화풋살 - A구장 [ 전체 대여 ] 9:00~11:00
+      <hr style={{color:'blue'}}/>
+    </li>
+      
+
+  )
+}
+
+export default NowBreakdown
