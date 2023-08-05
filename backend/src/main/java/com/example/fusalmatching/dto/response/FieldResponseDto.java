e5a395a78bfcd367f12efaf7b76e74ce3e96945c
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-08-05 20:43:09 +0900

    #53 - 매칭기능구현을 위한 요청DTO

--- backend/src/main/java/com/example/fusalmatching/dto/response/FieldResponseDto.java
+++ backend/src/main/java/com/example/fusalmatching/dto/response/FieldResponseDto.java
@@ -1,9 +1,12 @@
 package com.example.fusalmatching.dto.response;
 
 import com.example.fusalmatching.domain.Stadium;
+import com.example.fusalmatching.domain.Team;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +18,20 @@ public class FieldResponseDto {
     private String matchingDate;
     private String startTime;
     private String endTime;
+    private Long matchingId;
+    private List<TeamDto> team;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TeamDto {
+        private String id;
+        private String teamName;
+        private int manner;
+        private int skill;
+
+
+    }
 
 
 }
