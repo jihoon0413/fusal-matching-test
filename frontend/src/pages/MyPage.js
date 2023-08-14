39ceb8763b169f2bf1515e622bab94e89f14251d
minji-2219 <aabb0219@naver.com>
2023-08-14 23:37:07 +0900

    #65 - mypageBottom component화 하기

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -7,6 +7,7 @@ import axios from 'axios'
 import { changeText } from '../helper/ChangeText'
 import profile from '../img/ball.png'
 import { FutureBDContext } from '../context/FutureBreakDownContext'
+import MypageBottom from '../components/MypageBottom'
 
 const MyPage = () => {
 
@@ -16,26 +17,27 @@ const MyPage = () => {
   const year = date.getFullYear()
   const month = date.getMonth()+1
   const day = date.getDate()
-  const {futureBD,setFutureBD} = useContext(FutureBDContext)
-  useEffect(()=>{
-    const teamFetch = async()=>{
-      try{
-        const result = await axios.get(`https://6f2b-121-147-100-85.ngrok-free.app/teams?id=${idData}`,{
-        headers: {
-              'Content-Type': `application/json`,
-              'ngrok-skip-browser-warning': '69420',
-            },
-        })
-        setTeam(result.data)
-      }catch(err){
-        console.log("err입니당~",err)
-      }
+  const {futureBD,BDtitle} = useContext(FutureBDContext)
+
+  const teamFetch = async()=>{
+    try{
+      const result = await axios.get(`https://6f2b-121-147-100-85.ngrok-free.app/teams?id=${idData}`,{
+      headers: {
+            'Content-Type': `application/json`,
+            'ngrok-skip-browser-warning': '69420',
+          },
+      })
+      setTeam(result.data)
+    }catch(err){
+      console.log("err입니당~",err)
     }
+  }
+  useEffect(()=>{
     teamFetch()
   },[])
 
   console.log(team)
- 
+
   return (
     <>
     <div className='center'>
@@ -75,7 +77,6 @@ const MyPage = () => {
               let matDay = matchingRecord?.field.matchingDate.slice(8,10)
               if(Number(matYear)<year){
                 //현재 보다 matching년도가 전일때
-
               }
               else if(Number(matYear) === year){
                 //->현재보다 matching월이 전일 때
@@ -97,7 +98,7 @@ const MyPage = () => {
                       endTime={matchingRecord?.field.endTime}
                       teamId={team?.id}
                       matchingId={matchingRecord?.field.matchingId}
-
+                      teamFetch={teamFetch}
                       />)
                   }
                 }
@@ -113,6 +114,7 @@ const MyPage = () => {
                     endTime={matchingRecord?.field.endTime}
                     teamId={team?.id}
                     matchingId={matchingRecord?.field.matchingId}
+                    teamFetch={teamFetch}
                     />)
                 }}
                 //현재보다 mathing 년도가 빠를 때
@@ -127,6 +129,7 @@ const MyPage = () => {
                   endTime={matchingRecord?.field.endTime}
                   teamId={team?.id}
                   matchingId={matchingRecord?.field.matchingId}
+                  teamFetch={teamFetch}
                   />)
               }
             })}
@@ -146,7 +149,8 @@ const MyPage = () => {
               if(Number(matYear)<year){
                 return (<FutureBreakdown 
                   date={matchingRecord?.field.matchingDate} 
-                  stadium={matchingRecord?.field.stadiumName} 
+                  stadiumName={matchingRecord?.field.stadiumName} 
+                  stadiumId={matchingRecord?.field.stadiumId} 
                   fieldNum={matchingRecord?.field.fieldNum} 
                   allRental={matchingRecord?.field.allRental}
                   team={matchingRecord?.field?.team}
@@ -160,7 +164,8 @@ const MyPage = () => {
                 if(Number(matMonth) <month){ 
                   return (<FutureBreakdown 
                     date={matchingRecord?.field.matchingDate} 
-                    stadium={matchingRecord?.field.stadiumName} 
+                    stadiumName={matchingRecord?.field.stadiumName} 
+                    stadiumId={matchingRecord?.field.stadiumId} 
                     fieldNum={matchingRecord?.field.fieldNum} 
                     allRental={matchingRecord?.field.allRental}
                     team={matchingRecord?.field?.team}
@@ -173,7 +178,8 @@ const MyPage = () => {
                   if(Number(matDay)<day){
                     return (<FutureBreakdown 
                       date={matchingRecord?.field.matchingDate} 
-                      stadium={matchingRecord?.field.stadiumName} 
+                      stadiumName={matchingRecord?.field.stadiumName} 
+                      stadiumId={matchingRecord?.field.stadiumId} 
                       fieldNum={matchingRecord?.field.fieldNum} 
                       allRental={matchingRecord?.field.allRental}
                       team={matchingRecord?.field?.team}
@@ -195,87 +201,8 @@ const MyPage = () => {
           </div>
       </div>
       </div>
-
-
     </div>
-    {!futureBD
-      ?
-      <div className='center'>
-        <div className='mypage_bottom_toggle'>
-          <div className='toggle_btn'></div>
-
-          <div style={{marginTop:'20px',display:'flex',alignItems:'center',justifyContent:'center', height:'300px'}}>
-
-            <div className='stadium_review'>
-              <span className='review_mark'>구장 리뷰</span>
-              <span style={{fontWeight:'700'}}>23.06.01 abc풋살 - 맥시멈스 vs 미르 14:00~16:00</span>
-              <hr style={{width:'400px' ,margin:'5px 0 0 85px'}}/>
-              <div style={{marginTop:'30px'}}>
-                <input placeholder='리뷰를 작성해 주세요.' style={{height:'50px'}}/>
-                <button style={{marginLeft:'10px',backgroundColor:'#4287EE',color:'white'}}>등록</button>
-              </div>
-            </div>
-
-            <div className='team_review'>
-              <span className='review_mark'>팀 리뷰</span>
-              <span style={{fontWeight:'700'}}>23.06.01 abc풋살 - 맥시멈스 vs 미르 14:00~16:00</span>
-              <hr style={{width:'500px' ,margin:'5px 0 0 70px'}}/>
-              <div className='points'>
-              <div className='point'>
-                <span className='title'>🏆 실력 </span>
-                <button className='point_btn' style={{width:'30px', height:'30px'}}></button>
-                <button className='point_btn' style={{width:'25px', height:'25px'}}></button>
-                <button className='point_btn' style={{width:'20px', height:'20px'}}></button>
-                <button className='point_btn' style={{width:'25px', height:'25px'}}></button>
-                <button className='point_btn' style={{width:'20px', height:'20px'}}></button>
-                <button className='point_btn' style={{width:'25px', height:'25px'}}></button>
-                <button className='point_btn' style={{width:'30px', height:'30px'}}></button>
-              </div>
-              <hr className='pointbar'/>
-              <div className='point_ment'>
-                <div>훈련이 필요해보여요 !</div>
-                <div>좋아요 !</div>
-                <div style={{marginLeft:'20px'}}>와우, 프로인가요?</div>
-              </div>
-
-              <div className='point'>
-                <span className='title'>⭐ 매너 </span>
-                <button className='point_btn' style={{width:'30px', height:'30px'}}></button>
-                <button className='point_btn' style={{width:'25px', height:'25px'}}></button>
-                <button className='point_btn' style={{width:'20px', height:'20px'}}></button>
-                <button className='point_btn' style={{width:'25px', height:'25px'}}></button>
-                <button className='point_btn' style={{width:'20px', height:'20px'}}></button>
-                <button className='point_btn' style={{width:'25px', height:'25px'}}></button>
-                <button className='point_btn' style={{width:'30px', height:'30px'}}></button>
-              </div>
-              <hr className='pointbar2'/>
-              <div className='point_ment2'>
-                <div>매너가 아주 꽝 !</div>
-                <div>좋아요 !</div>
-                <div>최고의 매너예요!</div>
-              </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      :<div className='mypage_bottom'>
-        <ul className='bottom_ul'>
-          <ul className='bottom_ul_ul' style={{marginBottom:'10px'}}>
-            <li><span style={{fontWeight:'700'}}>상호명</span> (주) JNU산공이공<span style={{margin:'0 10px'}}>|</span></li>
-            <li>기획자 : 이은경<span style={{margin:'0 10px'}}>|</span></li>
-            <li>FE : 서민지<span style={{margin:'0 10px'}}>|</span></li>
-            <li>BE : 신지훈</li>
-          </ul>
-        
-        <li style={{marginBottom:'10px'}}><span style={{fontWeight:'700'}}>주소 :</span> 광주 북구 용봉로 77, 자동차공학관(공과대학 1호관)</li>
-        <li style={{marginBottom:'80px'}}><span style={{fontWeight:'700'}}>고객지원 :</span> 이메일 (luk0992@naver.com )</li>
-        <li style={{color:'#4287EE'}}>Copyright © JNU산공이공</li>
-        </ul>
-      </div>
-      
-      }
+    <MypageBottom/>
   </>  
   )
 }
