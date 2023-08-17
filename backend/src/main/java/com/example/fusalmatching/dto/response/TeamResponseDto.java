5cf7d4878c696cb25c0778d3aca75eb03a6683f8
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2023-08-17 20:51:07 +0900

    #71 - 마이 페이지 리턴 데이터 수정
    기존에 필드 조회와 같은 리턴 구성을 사용하였지만 생각보다 필요한 데이터가 달라 따로 만들어 리턴하기로 함

--- backend/src/main/java/com/example/fusalmatching/dto/response/TeamResponseDto.java
+++ backend/src/main/java/com/example/fusalmatching/dto/response/TeamResponseDto.java
@@ -17,15 +17,61 @@ public class TeamResponseDto {
     private String imageUrl;
     private int manner;
     private int skill;
-    private List<MatchingRecordDto> matchingRecordList;
+    private List<MatchingRecordListDto> matchingRecordList;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MatchingRecordListDto {
+        private MatchingRecordDto matchingRecord;
+    }
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class MatchingRecordDto {
-        private FieldResponseDto field;
+
+        private Long id;
+        private Long stadiumId;
+        private String stadiumName;
+        private String matchingDate;
+        private String startTime;
+        private String endTime;
+        private Long matchingId;
+        private int fieldNum;
+        private boolean confirm;
+        private boolean allRental;
+        private MyTeamDto myTeamDto;
+        private OppositeTeamDto oppositeTeamDto;
     }
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MyTeamDto {
+        private String id;
+//        private String teamName;
+//        private String imageUrl;
+//        private int manner;
+//        private int skill;
+        private Long teamMatchingId;
+        private boolean evalOpposite;
+        private boolean evalStadium;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OppositeTeamDto {
+        private String id;
+        private String teamName;
+        private String imageUrl;
+        private int manner;
+        private int skill;
+//        private Long teamMatchingId;
+//        private boolean evalOpposite;
+//        private boolean evalStadium;
+    }
 
 
 }
